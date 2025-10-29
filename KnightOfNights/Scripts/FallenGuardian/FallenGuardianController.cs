5fffafb15c6cb3d43c103a03ecfd7273540b9d46
dplochcoder <daniel.ploch.coder@gmail.com>
2025-10-29 16:35:01 -0400

    Implement attack selection

--- KnightOfNights/Scripts/FallenGuardian/FallenGuardianController.cs
+++ KnightOfNights/Scripts/FallenGuardian/FallenGuardianController.cs
@@ -1,10 +1,45 @@
-﻿using KnightOfNights.Scripts.SharedLib;
+﻿using KnightOfNights.Scripts.InternalLib;
+using KnightOfNights.Scripts.SharedLib;
 using PurenailCore.CollectionUtil;
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace KnightOfNights.Scripts.FallenGuardian;
+
+[Shim]
+internal enum AttackChoice
+{
+    AxeHopscotch,
+    GorbFireworks,
+    GorbNuclear,
+    PancakeWaveSlash,
+    PancakeDiveStorm,
+    ShieldCyclone,
+    UltraInstinct,
+    XeroArmada
+}
+
+[Shim]
+internal class FallenGuardianAttack : MonoBehaviour
+{
+    [ShimField] public AttackChoice Choice;
+    [ShimField] public float Weight;
+    [ShimField] public int Cooldown;
+    [ShimField] public float WeightIncrease;
+    [ShimField] public List<AttackChoice> ForbiddenPredecessors = [];
+}
+
+[Shim]
+internal class FallenGuardianPhaseStats : MonoBehaviour
+{
+    [ShimField] public int MinHP;
+    [ShimField] public AttackChoice FirstAttack;
+    [ShimField] public List<FallenGuardianAttack> Attacks = [];
+
+    internal bool DidFirstAttack = false;
+}
 
 [Shim]
 internal class FallenGuardianController : MonoBehaviour
@@ -16,11 +51,26 @@ internal class FallenGuardianController : MonoBehaviour
     [ShimField] public float ShortWait;
     [ShimField] public float VeryShortWait;
     [ShimField] public float SplitOffset;
+    [ShimField] public float EscalationPause;
+    [ShimField] public int StaggerCount;
 
-    private void OnEnable() => StartCoroutine(RunBoss());
+    [ShimField] public int HP;
+    [ShimField] public List<FallenGuardianPhaseStats> PhaseStats = [];
+
+    private FallenGuardianPhaseStats? stats;
+
+    private void OnEnable() => this.StartLibCoroutine(RunBoss());
+
+    private void Update() => stats = PhaseStats.Where(s => HP >= s.MinHP).First();
 
     private IEnumerator<SlashAttackSequence> SpecTutorial()
     {
+        yield return new FlippableSlashAttackSequence([
+            (0f, SlashAttackSpec.LEFT.WithTelegraph(Telegraph)),
+            (LongWait, SlashAttackSpec.RIGHT.WithTelegraph(Telegraph)),
+            (LongWait, SlashAttackSpec.LEFT.WithTelegraph(Telegraph))
+        ]);
+
         yield return new FlippableSlashAttackSequence([
             (0f, SlashAttackSpec.LEFT.WithTelegraph(Telegraph)),
             (LongWait, SlashAttackSpec.RIGHT.WithTelegraph(Telegraph)),
@@ -48,21 +98,14 @@ internal class FallenGuardianController : MonoBehaviour
             (LongWait + ShortWait, SlashAttackSpec.RIGHT.WithTelegraph(Telegraph)),
             (0f, SlashAttackSpec.LEFT.WithTelegraph(Telegraph + ShortWait)),
             (2 * ShortWait, SlashAttackSpec.HIGH_LEFT.WithTelegraph(Telegraph)),
-            (0f, SlashAttackSpec.HIGH_RIGHT.WithTelegraph(Telegraph))
-        ]);
-
-        yield return new FlippableSlashAttackSequence([
-            (0f, SlashAttackSpec.LEFT.Up(SplitOffset).WithTelegraph(Telegraph)),
-            (VeryShortWait, SlashAttackSpec.RIGHT.Up(SplitOffset).WithTelegraph(Telegraph)),
-            (VeryShortWait, SlashAttackSpec.HIGH_LEFT.WithTelegraph(Telegraph)),
-            (VeryShortWait, SlashAttackSpec.RIGHT.Down(SplitOffset).WithTelegraph(Telegraph)),
-            (VeryShortWait, SlashAttackSpec.LEFT.Down(SplitOffset).WithTelegraph(Telegraph)),
-            (VeryShortWait, SlashAttackSpec.HIGH_RIGHT.WithTelegraph(Telegraph))
+            (0f, SlashAttackSpec.HIGH_RIGHT.WithTelegraph(Telegraph + ShortWait))
         ]);
     }
 
-    private IEnumerator RunBoss()
+    private IEnumerator<CoroutineElement> RunBoss()
     {
+        // TODO: Aura farm intro.
+
         IEnumerator<SlashAttackSequence> tutorial = SpecTutorial();
         while (tutorial.MoveNext())
         {
@@ -70,15 +113,109 @@ internal class FallenGuardianController : MonoBehaviour
 
             while (true)
             {
-                yield return new WaitForSeconds(SequenceDelay);
+                yield return Coroutines.SleepSeconds(SequenceDelay);
 
                 Wrapped<SlashAttackResult> result = new(SlashAttackResult.PENDING);
                 sequence.Play(r => result.Value = r);
 
-                yield return new WaitUntil(() => result.Value != SlashAttackResult.PENDING);
+                yield return Coroutines.SleepUntil(() => result.Value != SlashAttackResult.PENDING);
                 if (result.Value == SlashAttackResult.NOT_PARRIED) continue;
                 else break;
             }
+        }
+
+        yield return Coroutines.SleepSeconds(EscalationPause);
+
+        AttackChoice previousAttack = AttackChoice.UltraInstinct;
+        while (true)
+        {
+            AttackChoice attack;
+            if (!stats!.DidFirstAttack) attack = stats!.FirstAttack;
+            else attack = ChooseAttack(previousAttack);
+
+            RecordChoice(attack);
+            var oneof = Coroutines.OneOf(
+                Coroutines.Sequence(ExecuteAttack(attack)),
+                Coroutines.SleepUntil(() => HP <= 0),
+                Coroutines.SleepUntil(() => multiParries >= StaggerCount));
+            yield return oneof;
+
+            if (oneof.Choice == 1)
+            {
+                OnDeath?.Invoke();
+                yield break;
+            }
+
+            if (oneof.Choice == 2)
+            {
+                OnStagger?.Invoke();
+                OnStagger = null;
+                multiParries = 0;
+            }
+
+            // Continue to next attack.
+        }
+    }
+
+    private int multiParries;
+
+    internal event Action? OnDeath;  // Invoked once.
+    private event Action? OnStagger;  // Cleared after use.
+
+    private readonly HashMultiset<AttackChoice> Cooldowns = new();
+    private readonly HashMultiset<AttackChoice> WeightAdditions = new();
+
+    private void RecordChoice(AttackChoice choice)
+    {
+        foreach (var c in stats!.Attacks)
+        {
+            if (c.Choice == choice)
+            {
+                WeightAdditions.RemoveAll(c.Choice);
+                Cooldowns.Add(c.Choice, c.Cooldown);
+            }
+            else
+            {
+                if (Cooldowns.CountOf(c.Choice) > 0) Cooldowns.Remove(c.Choice);
+                else WeightAdditions.Add(c.Choice);
+            }
+        }
+    }
+
+    private AttackChoice ChooseAttack(AttackChoice previous)
+    {
+        IndexedWeightedSet<AttackChoice> choices = new();
+        foreach (var c in stats!.Attacks)
+        {
+            if (previous == c.Choice) continue;
+            if (Cooldowns.CountOf(c.Choice) > 0) continue;
+            if (c.ForbiddenPredecessors.Contains(previous)) continue;
+
+            choices.Add(c.Choice, c.Weight + c.WeightIncrease * WeightAdditions.CountOf(c.Choice));
+        }
+        return choices.Sample();
+    }
+
+    private IEnumerator<CoroutineElement> ExecuteAttack(AttackChoice choice)
+    {
+        switch (choice)
+        {
+            case AttackChoice.AxeHopscotch:
+                yield break;
+            case AttackChoice.GorbFireworks:
+                yield break;
+            case AttackChoice.GorbNuclear:
+                yield break;
+            case AttackChoice.PancakeDiveStorm:
+                yield break;
+            case AttackChoice.PancakeWaveSlash:
+                yield break;
+            case AttackChoice.ShieldCyclone:
+                yield break;
+            case AttackChoice.UltraInstinct:
+                yield break;
+            case AttackChoice.XeroArmada:
+                yield break;
         }
     }
 }
