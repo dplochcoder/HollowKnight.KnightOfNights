f0edc8f17ccf03cfe31f92c1f84f4c06035bc83c
dplochcoder <daniel.ploch.coder@gmail.com>
2025-11-01 22:10:05 -0400

    Fix up sprite flash

--- KnightOfNights/Scripts/Proxy/EnemyHitEffectsGhostProxy.cs
+++ KnightOfNights/Scripts/Proxy/EnemyHitEffectsGhostProxy.cs
@@ -1,5 +1,7 @@
 ﻿using KnightOfNights.Scripts.InternalLib;
 using KnightOfNights.Scripts.SharedLib;
+using SFCore.Utils;
+using UnityEngine;
 
 namespace KnightOfNights.Scripts.Proxy;
 
@@ -14,9 +16,14 @@ internal class EnemyHitEffectsGhostProxy : EnemyHitEffectsGhost
         "slashEffectGhost1",
         "slashEffectGhost2");
 
+    [ShimField] public GameObject? SpriteFlashOverride;
+
     protected new void Awake()
     {
         base.Awake();
         Patcher.Patch(this);
+
+        var spriteFlash = SpriteFlashOverride?.GetComponent<SpriteFlash>();
+        if (spriteFlash != null) this.SetAttr("spriteFlash", spriteFlash);
     }
 }
