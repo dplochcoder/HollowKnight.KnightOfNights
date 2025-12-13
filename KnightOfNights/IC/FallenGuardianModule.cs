4b9c9321ab7d09bb009705384e393d2bc0f0c98f
Daniel Ploch <daniel.ploch.coder@gmail.com>
2025-12-13 01:53:45 -0500

    Debug scene load hook doesn't work

--- KnightOfNights/IC/FallenGuardianModule.cs
+++ KnightOfNights/IC/FallenGuardianModule.cs
@@ -56,7 +56,7 @@ internal class FallenGuardianModule : AbstractModule<FallenGuardianModule>
     private void OnBeforeSceneLoad(string sceneName, Action cb)
     {
         var assetBundleName = AssetBundleName(sceneName);
-        if (!sceneBundles.ContainsKey(assetBundleName) && !DebugAssetExists(assetBundleName))
+        if (!sceneBundles.ContainsKey(assetBundleName))
         {
             cb();
             return;
@@ -75,16 +75,6 @@ internal class FallenGuardianModule : AbstractModule<FallenGuardianModule>
             assetBundle?.Unload(true);
             sceneBundles[assetBundleName] = null;
         }
-    }
-
-    private bool DebugAssetExists(string name)
-    {
-#if DEBUG
-        var debugData = DebugData.Get();
-        return File.Exists($"{debugData.LocalAssetBundlesPath}/{name}");
-#else
-        return false;
-#endif
     }
 
     private AssetBundleCreateRequest LoadAssetAsync(string name)
