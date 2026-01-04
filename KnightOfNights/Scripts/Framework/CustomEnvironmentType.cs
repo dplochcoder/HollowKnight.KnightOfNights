103b9aac75fd6c312eafbd5a6d4fa90121f512a0
Daniel Ploch <daniel.ploch.coder@gmail.com>
2026-01-04 13:59:30 -0500

    Decorate entry hall

--- KnightOfNights/Scripts/Framework/CustomEnvironmentType.cs
+++ KnightOfNights/Scripts/Framework/CustomEnvironmentType.cs
@@ -5,11 +5,17 @@ namespace KnightOfNights.Scripts.Framework;
 [Shim]
 internal enum CustomEnvironmentType
 {
+    METAL = 4,
     SNOW = 12,  // This cannot change even if `ToIntId()` changes.
     ICE = 13,   // This cannot change even if `ToIntId()` changes.
 }
 
 internal static class CustomEnvironmentExtensions
 {
-    internal static int ToIntId(this CustomEnvironmentType self) => self switch { CustomEnvironmentType.SNOW => 12, CustomEnvironmentType.ICE => 13, _ => throw self.InvalidEnum() };
+    internal static int ToIntId(this CustomEnvironmentType self) => self switch {
+        CustomEnvironmentType.METAL => 4,
+        CustomEnvironmentType.SNOW => 12,
+        CustomEnvironmentType.ICE => 13,
+        _ => throw self.InvalidEnum()
+    };
 }
--- UnityScriptShims/Scripts/Generated/Framework/CustomEnvironmentType.cs
+++ UnityScriptShims/Scripts/Generated/Framework/CustomEnvironmentType.cs
@@ -2,6 +2,7 @@ namespace KnightOfNights.Scripts.Framework
 {
     public enum CustomEnvironmentType
     {
+        METAL = 4,
         SNOW = 12,
         ICE = 13,
         
