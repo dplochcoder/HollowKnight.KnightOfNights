19faa74f7501bfbafbee41dce580c77769d29f90
Daniel Ploch <daniel.ploch.coder@gmail.com>
2026-01-12 02:28:44 -0500

    Fix dumbdumb TC spaghetti

--- KnightOfNights/IC/SummitSceneNames.cs
+++ KnightOfNights/IC/SummitSceneNames.cs
@@ -3,10 +3,10 @@ namespace KnightOfNights.IC;
 internal static class SummitSceneNames
 {
     public const string Summit_BigWindClimb = "Summit_BigWindClimb";
-    public const string Summit_Boss = "Summit_Boss";
     public const string Summit_Checkpoint = "Summit_Checkpoint";
     public const string Summit_EntryHall = "Summit_EntryHall";
     public const string Summit_EntryPlain = "Summit_EntryPlain";
+    public const string Summit_Final = "Summit_Final";
     public const string Summit_Intermission = "Summit_Intermission";
     public const string Summit_SpikeTunnels = "Summit_SpikeTunnels";
     public const string Summit_TransCliffs = "Summit_TransCliffs";
